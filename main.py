25edc69efbb9a8aad0424e2d83c03e24c77f4448
Deepansh Dubey <deepansh.dubey20@gmail.com>
2020-08-18 15:07:13 +0530

    Many improvements

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
 import pymysql
+from flask_mail import Mail
+from werkzeug.utils import secure_filename
+import os
+import math
 pymysql.install_as_MySQLdb()
 
 with open('C:/Users/fangs/PycharmProjects/blogsite/templates/config.json', 'r') as c:
     params = json.load(c)["params"]
 local_server = True
 app = Flask(__name__)
+app.secret_key = 'super-secret-key'
+app.config['UPLOAD_FOLDER'] = params['upload_location']
+# app.config.update(
+#     MAIL_SERVER = 'smtp.gmail.com',
+#     MAIL_POST = '465',
+#     MAIL_USE_SSL = True,
+#     MAIL_USERNAME = params['gmail-user'],
+#     MAIL_PASSWORD = params['gmail-password']
+# )
+
+mail = Mail(app)
+
 if local_server:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 else:
@@ -29,20 +45,122 @@ class Posts(db.Model):
     title = db.Column(db.String(80), unique=False, nullable=False)
     slug = db.Column(db.String(30),  nullable=False)
     content = db.Column(db.String(200),  nullable=False)
+    tagline = db.Column(db.String(12), nullable=True)
     date = db.Column(db.String(12),  nullable=True)
     img_name = db.Column(db.String(12), nullable=True)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+    page = request.args.get('page')
+    page=int(page)
+    if (not str(page).isnumeric()):
+        page = 1
+    posts = posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+int(params['no_of_posts'])]
+    # firstpage
+    if page == 1:
+        prev="#"
+        next = "/?page="+ str(page + 1)
+    elif page == last:
+        prev = "/?page=" + str(page - 1)
+        next =  "#"
+    else:
+        prev = "/?page=" + str(page - 1)
+        next = "/?page="+ str(page + 1)
+
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 @app.route("/about")
 def about():
     return render_template('about.html', params=params)
 
+@app.route("/uploader", methods=['GET', 'POST'])
+def uploader():
+    if 'user' in session and session['user'] == params['admin_user']:
+        if(request.method == 'POST'):
+            f = request.files['file1']
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            return "Uploaded Successfully"
+
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    session.pop('user')
+    return redirect('/dashboard')
+
+@app.route("/delete/<string:sno>", methods=['GET', 'POST'])
+def delete(sno):
+    if 'user' in session and session['user'] == params['admin_user']:
+        post = Posts.query.filter_by(sno = sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect('/dashboard')
+
+
+@app.route("/dashboard", methods=['GET', 'POST'])
+def dashboard():
+
+    if 'user' in session and session['user'] == params['admin_user']:
+        posts = Posts.query.all()
+        return render_template('dashboard.html', posts=posts, params=params)
+    if request.method == 'POST':
+        username = request.form.get('uname')
+        userpass = request.form.get('pass')
+        if username == params['admin_user'] and userpass == params['admin_password']:
+            session['user'] = username
+            posts = Posts.query.all()
+            return render_template('dashboard.html', posts=posts, params=params)
+
+
+    else:
+
+        return render_template('login.html', params=params)
+
+
+@app.route("/edit/<string:sno>", methods=['GET', 'POST'])
+def edit(sno):
+    if 'user' in session and session['user'] == params['admin_user']:
+        if request.method == 'POST':
+            box_title = request.form.get('title')
+            tline = request.form.get('tline')
+            slug = request.form.get('slug')
+            content = request.form.get('content')
+            img_file = request.form.get('img_file')
+            date = datetime.now()
+
+            if sno == '0':
+                post = Posts(title=box_title,
+                             slug=slug,
+                             content=content,
+                             tagline=tline,
+                             img_file=img_file,
+                             sno=sno,
+                             date=date)
+                db.session.add(post)
+                db.session.commit()
+            else:
+                post = Posts.query.filter_by(sno=sno).first()
+                post.title = box_title
+                post.slug = slug
+                post.tagline = tline
+                post.img_file = img_file
+                post.date = date
+                db.session.commit()
+                return redirect('/edit/'+sno)
+
+        post = Posts.query.filter_by(sno=sno).first()
+        return render_template('edit.html', params=params, sno=sno, post=post)
+
+
 @app.route("/post/<string:post_slug>", methods=['GET'])
-def post_route(post_slug):
+@app.route("/post/")
+def post_route(post_slug=None):
+    if (post_slug == None):
+        return 'fangs'
+
     post = Posts.query.filter_by(slug=post_slug).first()
 
     return render_template('post.html', params=params, post=post)
@@ -59,6 +177,11 @@ def contact():
         entry = Contacts(name=name, email=email, phone=phone, msg=message, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
+        # mail.send_message('New message from' + name,
+        #                   sender=email,
+        #                   recipients=[params['gmail-user']],
+        #                   body=message + "\n" + phone
+        #                   )
     return render_template('contact.html')
 
-app.run(debug = True)
+app.run(debug=True)
